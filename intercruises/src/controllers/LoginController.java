4d73914944883cc54b2fc667f1edc4b8e3f1a59c
Your Name <you@example.com>
2020-03-11 18:35:13 +0100

    test login

--- intercruises/src/controllers/LoginController.java
+++ intercruises/src/controllers/LoginController.java
@@ -1,14 +1,16 @@
 package controllers;
 
-import java.awt.TextField;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import services.LoginService;
 
 public class LoginController implements Initializable{
 	
@@ -20,20 +22,19 @@ public class LoginController implements Initializable{
 	
 	@FXML
 	private PasswordField password;
+	
+	@FXML
+	private Button loginButton;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		
 		title.setImage(new Image("file:src/img/intercruises.png"));
-		
 	}
 	
-	public boolean login () {
-		
-		
-		
-		
-		return true;
+	@FXML
+	void login() {
+		new LoginService().checkCreds(email.getText(), password.getText());
 	}
 
 	
--- intercruises/src/services/LoginService.java
+++ intercruises/src/services/LoginService.java
@@ -1,5 +1,57 @@
 package services;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+
+import com.google.gson.Gson;
+
 public class LoginService {
+
+	String url = "http://localhost:3000/login";
+
+	public void checkCreds(String username, String password) {
+		HashMap<String, String> mapa = new HashMap<String, String>();
+		mapa.put("username", username);
+		mapa.put("password", password);
+
+		HttpURLConnection conn = null;
+
+		try {
+			conn = (HttpURLConnection) new URL(url).openConnection();
+
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Content-Type", "application/json; utf-8");
+			conn.setRequestProperty("Accept", "application/json");
+			conn.setDoOutput(true);
+
+			String body = new Gson().toJson(mapa);
+
+			System.out.println(body);
+
+			try (OutputStream os = conn.getOutputStream()) {
+				byte[] input = body.getBytes("utf-8");
+				os.write(input, 0, input.length);
+			}
+
+			try (BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"))) {
+				StringBuilder response = new StringBuilder();
+				String responseLine = null;
+				while ((responseLine = br.readLine()) != null) {
+					response.append(responseLine.trim());
+				}
+				System.out.println(response.toString());
+			}
+			
+			conn.disconnect();
+		} catch (IOException e) {
+			System.out.println("IOException-LoginService.checkCreds: " + e.getMessage());
+		}
+
+	}
 
 }
